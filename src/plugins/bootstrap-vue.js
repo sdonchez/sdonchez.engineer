628fc492b753868a3fd9fb9bb29b8681234f67f4
Stephen Donchez <sdonchez@villanova.edu>
2025-02-27 21:06:20 -0500

    Move PrimeVue preset to own file

--- src/plugins/bootstrap-vue.js
+++
@@ -1,6 +0,0 @@
-//import '../custom.scss'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'bootswatch/dist/darkly/bootstrap.min.css'
-
-export default BootstrapVue; // Export the BootstrapVue3 plugin for use in main.js
---
+++ src/plugins/primeVue.js
@@ -0,0 +1,55 @@
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes'
+
+const SGDPreset = definePreset(Aura, {
+	//define new color palette centered around the primary color from the old site
+	primitive: {
+		bahama: { 
+			50: '#879cb2', 
+			100: '#738ca5', 
+			200: '#5f7b99', 
+			300: '#4b6b8c', 
+			400: '#375a7f', //primary from darkly
+			500: '#325172', 
+			600: '#2c4866', 
+			700: '#273f59', 
+			800: '#21364c', 
+			900: '#1c2d40', 
+			950: '#162433'}
+	},
+	//make this the primary palette
+	semantic: {
+        primary: {
+            50: '{bahama.50}',
+            100: '{bahama.100}',
+            200: '{bahama.200}',
+            300: '{bahama.300}',
+            400: '{bahama.400}', //used by bg-primary
+            500: '{bahama.500}',
+            600: '{bahama.600}',
+            700: '{bahama.700}',
+            800: '{bahama.800}',
+            900: '{bahama.900}',
+            950: '{bahama.950}'
+        }
+    },
+	components: {
+		menubar: {
+			root: {
+				background: '{primary.400}',
+			},
+			item: {
+				focusBackground: '{primary.600}'
+			},
+			mobileButton:{
+				hoverBackground: '{primary.600}',
+				color: '{white}'
+			},
+			submenu:{
+				background: '{primary.400}'
+			}
+		},
+	}
+});
+
+export default SGDPreset;
