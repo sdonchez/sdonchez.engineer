aed861709178aca975153f73125af3104f2c785e
Stephen Donchez <sdonchez@villanova.edu>
2025-02-28 22:40:24 -0500

    Functional Menubar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,40 +2,58 @@
 	<div>
 		<Menubar :model="items" class="flex flex-row flex-nowrap !py-4">
 			<template #start><a class="flex flex-row items-center name-title" href="/" target="_self"><Avatar image="favicon-32x32.png" class="mx-4" size="32px"></Avatar>Stephen G. Donchez</a></template>
+			<template #item="{ item, props, hasSubmenu }">
+			<!--According to the PrimeVue docs, none of these item or props references should have to be optional, 
+			but everything crashes and burns if they aren't. Several hours of debugging, stack tracing, 
+			and arguing with chatgpt got me nowhere, but this seems to work for some reason.
+			
+			There's no logical reason for it either - I've dumped the entire Items array, and there are no elements 
+			that don't contain both route and label. Based on that, the v-else should never execute either, 
+			but it crashes if those aren't optional too, so clearly something screwy is going on.-->
+			
+			<router-link v-if="item?.route" v-slot="{ href, navigate }" :to="item?.route" custom>
+				<a v-ripple :href="href" v-bind="props.action" @click="navigate">
+					<span>{{ item.label }}</span>
+				</a>
+			</router-link>
+			<a v-else v-ripple :href="item?.url" :target="item?.target" v-bind="props?.action">
+				<span>{{ item?.label }}</span>
+				<span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+			</a>
+		</template>
 		</Menubar>
 	</div>
 </template>
 
 
 <script setup>
-import { ref } from 'vue';
-
-const items = ref([
+import Menubar from 'primevue/menubar';
+const items = [
   {
     label: 'Home',
-	to: '/'
+	route:'/home'
   },
   {
     label: 'Work Experience',
-	to: '/work'
+	route: '/work'
   },
   {
     label: 'Projects',
-	to: '/projects'
+	route: 'projects'
   },
   {
 	label: 'Publications',
-	to: '/publications'
+	route: 'publications'
   },
   {
 	label: 'Skills',
-	to: '/skills'
+	route: '/skills'
   },
   {
 	label: 'Certifications',
-	to: '/certifications'
+	route: 'certifications'
   }
-]);
+];
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import Home from '../views/Home.vue'
 import { createRouter as _createRouter, createWebHistory } from 'vue-router'
 
+const HomeView = () => import('../views/Home.vue')
 const WorkView = () => import('../views/Work.vue')
 const ProjectsView = () => import('../views/Projects.vue')
 const EducationView = () => import('../views/Education.vue')
@@ -8,20 +8,23 @@ const PublicationsView = () => import('../views/Publications.vue')
 const SkillsView = () => import('../views/Skills.vue')
 const CertificationsView = () => import('../views/Certifications.vue')
 
+const routes = [
+	{ path: '/', name: 'Home', component: HomeView },
+	{ path: '/work', name: 'Work Experience', component: WorkView },
+	// { path: '/projects', name: 'Projects', component: ProjectsView },
+	// { path: '/education', name: 'Education', component: EducationView },
+	// { path: '/publications', name: 'Publications', component: PublicationsView },
+	{ path: '/skills', name: 'Skills', component: SkillsView },
+	// { path: '/certifications', name: 'Certifications', component: CertificationsView },
+	{ path: '/home', redirect: '/' }
+
+]
+
 export function createRouter() {
 	return _createRouter({
 		base: import.meta.env.BASE_URL,
 		history: createWebHistory(),
 		scrollBehavior: () => ({ top: 0 }),
-		routes: [
-			{ path: '/', name: 'Home', component: Home },
-			// { path: '/work', name: 'Work Experience', component: WorkView },
-			// { path: '/projects', name: 'Projects', component: ProjectsView },
-			// { path: '/education', name: 'Education', component: EducationView },
-			// { path: '/publications', name: 'Publications', component: PublicationsView },
-			{ path: '/skills', name: 'Skills', component: SkillsView },
-			// { path: '/certifications', name: 'Certifications', component: CertificationsView },
-
-		]
+		routes: routes
 	})
 }
